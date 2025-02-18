10bd5ac9928afbf36206cce95afae80dba36ab0a
bthipireddy <bthipireddy@slu.edu>
2025-02-18 17:07:43 +0000

    The edit() and destroy() methods have been implemented in orders.js and registered in app.js, enabling order updates and deletions successfully.

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const Products = require('./products')
 const autoCatch = require('./lib/auto-catch')
-
+const Orders = require('./orders')
 /**
  * Handle the root route
  * @param {object} req
@@ -49,30 +49,56 @@ async function getProduct(req, res, next) {
  * @param {object} req 
  * @param {object} res 
  */
-async function createProduct(req, res) {
-  console.log('request body:', req.body)
-  res.json(req.body)
+async function createProduct (req, res, next) {
+  const product = await Products.create(req.body)
+  res.json(product)
+}
+
+async function editProduct (req, res, next) {
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+  res.json(product)
+}
+async function deleteProduct (req, res, next) {
+  const response = await Products.destroy(req.params.id)
+  res.json(response)
+}
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+  res.json(order)
+}
+
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({ 
+    offset: Number(offset), 
+    limit: Number(limit),
+    productId, 
+    status 
+  })
+
+  res.json(orders)
+}
+async function editOrder(req, res) {
+  try {
+    const updatedOrder = await orders.edit(req.params.id, req.body);
+    res.json(updatedOrder);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 }
 
 /**
- * Edit a product
- * @param {object} req
- * @param {object} res
- * @param {function} next
+ * Delete an order
  */
-async function editProduct(req, res, next) {
-  console.log(req.body)
-  res.json(req.body)
-}
-
-/**
- * Delete a product
- * @param {*} req 
- * @param {*} res 
- * @param {*} next 
- */
-async function deleteProduct(req, res, next) {
-  res.json({ success: true })
+async function deleteOrder(req, res) {
+  try {
+    await orders.destroy(req.params.id);
+    res.status(204).send(); // No content response
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 }
 
 module.exports = autoCatch({
@@ -81,5 +107,9 @@ module.exports = autoCatch({
   getProduct,
   createProduct,
   editProduct,
-  deleteProduct
+  deleteProduct,
+  listOrders,
+  createOrder,
+  editOrder,
+  deleteOrder
 });
